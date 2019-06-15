aa8c597120025041d78d145cdd3f4957a0b18c4d
Paulo Henrique Santos <paulohs.dev@gmail.com>
2019-06-15 06:39:06 -0300

    Update index.php
    Altera o método de ordenação

--- problema_i/index.php
+++ problema_i/index.php
@@ -1,13 +1,10 @@
 <?php
 $array = [[241, 121, 361, 501], 5, 6, 10, [103, 204, [305, 725, 666]]];
-function MaxArray($array){
-    // Array de numeros a ser ordenados 
-    $numbers = [];
-    // utilizo o wall recursivo passando a variavel $numbers por referência 
-    array_walk_recursive($array,function($item) use(&$numbers){
-        $numbers[] = $item;
-    });
-    // utilizando o metodo max para retornar o maior numero
-    return max($numbers);
-}
-echo MaxArray($array);
+// Array de numeros a ser ordenados 
+$max;
+// utilizo o wall recursivo passando a variavel $numbers por referência 
+array_walk_recursive($array,function($numero) use(&$max){
+	$max = ($numero > $max)? $numero : $max;
+});
+// utilizando o metodo max para retornar o maior numero
+echo $max;;
